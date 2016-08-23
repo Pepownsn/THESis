cb3bd22d3118625199d6a20d3f4361281b01b741
atlab <fsrr.wnsn@hotmail.com>
2016-08-23 13:45:57 +0700

    test-5

--- learning/test-5.py
+++ learning/test-5.py
@@ -1,3 +1,16 @@
-'''
-while loop 
-'''
+'''''''''''''''''''''
+>>>>>> loop <<<<<<<<
+'''''''''''''''''''''
+
+## while loop
+condition = 1
+while condition < 10:
+    print condition
+    condition += 1     # condition = condition +1
+
+while True :                #infinite loop (don't have condition) && and "True" must be the capital letter
+    print "burst loop"
+    print "a"
+
+
+## For loop
