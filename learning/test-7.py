01ab67562766daa7504a7856c1b8e151c2554128
atlab <fsrr.wnsn@hotmail.com>
2016-08-24 18:28:19 +0700

    test-7

--- learning/test-7.py
+++ learning/test-7.py
@@ -1,9 +1,18 @@
 #### FUNCTION
-''''
-
-''''
+'''
+test
+'''
 #define function: def 'name'(): add:
 def  example():
     print('basic function')
     z = 3 + 6 + 9 + 12 +15
     print(z)
+example()
+
+### Function Parameter
+def simple_addition (num1,num2):
+    answer = num1 + num2
+    print 'num1 is',num1
+    print answer
+simple_addition(7.5,10)
+simple_addition(num2 = 10,num1 = 80) ##type2
