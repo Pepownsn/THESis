f9dfe8032501384b4a3fd79a29dde85037dbafb7
atlab <fsrr.wnsn@hotmail.com>
2016-08-25 13:42:47 +0700

    test-7

--- learning/test-7.py
+++ learning/test-7.py
@@ -16,4 +16,25 @@ def simple_addition (num1,num2):
     print answer
 simple_addition(7.5,10)
 simple_addition(num2 = 10,num1 = 80) ##type2
-////////
+##not ok : simple_addition(num1 = 10)
+
+#global & local
+x = 6
+def example1():
+    global x    ##add to use x in function
+    x += 5
+    print(x)
+example1()
+
+## not ok : def example2():
+##    print (x)
+##    x += 7
+##    print (x)
+## example2()
+
+def example3():
+    print x    ##value of x is changed (6 to 11)
+    sum = x
+    sum += 20
+    print sum
+example3()
