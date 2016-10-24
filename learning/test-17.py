8ede0ea94b54345ae8fcce888755e3553f72215b
atlab <fsrr.wnsn@hotmail.com>
2016-10-25 01:38:57 +0700

    test

---
+++ learning/test-17.py
@@ -0,0 +1,17 @@
+## Multi-dimension list
+## 2-dimension
+x = [[5,6],[4,5],[6,4],[6,5],[4,6],[4,4],[6,1],[6,7],[8,7]] # 9row //1 2colume
+print x
+print x[0]
+print x[1][0],x[1][1]
+print x[1][0]
+
+## 3-dimension
+x = [
+    [[5,7],[5,5]],
+    [[6,9],[7,8]],
+    [[7,2],[2,5]]
+    ]
+print x[1]
+print x[1][0]
+print x[1][0][1]
