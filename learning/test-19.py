6165f5b7bb8c4014163532c6e0860332e26c765c
atlab <fsrr.wnsn@hotmail.com>
2016-10-25 02:02:14 +0700

    test

--- learning/test-19.py
+++ learning/test-19.py
@@ -1,12 +1,6 @@
 ## reading from a csv spreadsheet
-import sys, argparse, csv
 
-# command arguments
-parser = argparse.ArgumentParser(description='csv to postgres',\
-fromfile_prefix_chars="@" )
-parser.add_argument('file', help='csv file to import', action='store')
-args = parser.parse_args()
-csv_file = args.file
+
 
 # open csv file
 with open(csv_file, 'rb') as csvfile:
