61c891707009b05f5c55adacee94968dcec3ed48
atlab <fsrr.wnsn@hotmail.com>
2016-10-24 23:49:08 +0700

    test

--- learning/test-14.py
+++ learning/test-14.py
@@ -1,4 +1,12 @@
-## Making Module
+##listd and tuples
+## index of range start at 0
+x = 1,6,2,6
+x = (7,6,2,6)
+y = [5,6,2,6]
+print(x[0],y[3])
 
-def ex(data):
-    print(data)
+def exampleFunc():
+    return 15,6
+x,y = exampleFunc()
+print x
+print y
