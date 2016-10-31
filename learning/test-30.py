a18af56d87855f2cbdd4dc199e8d00059bd8820e
atlab <fsrr.wnsn@hotmail.com>
2016-10-31 20:46:15 +0700

    test

---
+++ learning/test-30.py
@@ -0,0 +1,28 @@
+'''
+FTP (file transfer protocol)
+download & upload
+'''
+from ftplib import FTP
+
+#domain name or server ip:
+ftp = FTP('123.server.ip')
+ftp.login(user='username', passwd = 'password')
+ftp.cwd('/whyfix/')
+
+
+##download
+def grabFile():
+
+    filename = 'example.txt'
+
+    localfile = open(filename, 'wb')
+    ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+
+    ftp.quit()
+    localfile.close()
+
+## upload file
+def placeFile():
+    filename = 'exampleFile.txt'
+    ftp.storbinary('STOR '+filename, open(filename, 'rb'))
+    ftp.quit()
---
+++ learning/test-31.py
@@ -0,0 +1,25 @@
+'''
+ Sockets intro
+'''
+import MySQLdb
+import this
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(s)
+server = 'http://google.co.th'
+port = 80
+
+server_ip = socket.gethostbyname(server)
+print(server_ip)
+
+request = "GET / HTTP/1.1\nHost: "+server+"\n\n"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("pythonprogramming.net", 80))
+
+s.send(request.encode())
+result = s.recv(4096)
+
+print(result)
+while (len(result) > 0):
+    print(result)
+    result = s.recv(4096)
